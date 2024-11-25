e535ab688d89d34293fa1a22f7a5e345923fa659
StudioKad <studiokad75@gmail.com>
2024-11-25 19:37:25 +0100

    update/react-table/search/sort

--- src/screens/ListEmployeeScreen/ListEmployeeScreen.jsx
+++ src/screens/ListEmployeeScreen/ListEmployeeScreen.jsx
@@ -1,8 +1,70 @@
+import React, { useMemo } from "react";
+import { useTable, useSortBy, useGlobalFilter } from "react-table";
 import { useSelector } from "react-redux";
-import { Table, Card } from "react-bootstrap";
+import { Table, Card, InputGroup, FormControl } from "react-bootstrap";
+import { FaSearch } from "react-icons/fa";
 
 const ListEmployeeScreen = () => {
   const employees = useSelector((state) => state.employees.employees);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "First Name",
+        accessor: "firstName",
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+      },
+      {
+        Header: "Date of Birth",
+        accessor: "dateOfBirth",
+      },
+      {
+        Header: "Start Date",
+        accessor: "startDate",
+      },
+      {
+        Header: "Department",
+        accessor: "department",
+      },
+      {
+        Header: "Street",
+        accessor: "street",
+      },
+      {
+        Header: "City",
+        accessor: "city",
+      },
+      {
+        Header: "State",
+        accessor: "state",
+      },
+      {
+        Header: "Zip Code",
+        accessor: "zipCode",
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state: { globalFilter },
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data: employees,
+    },
+    useGlobalFilter,
+    useSortBy
+  );
 
   return (
     <>
@@ -11,35 +73,55 @@ const ListEmployeeScreen = () => {
           Employee List
         </Card.Title>
         <Card.Body>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Search by name, department, or city"
+              value={globalFilter || ""}
+              onChange={(e) => setGlobalFilter(e.target.value)}
+            />
+            <InputGroup.Text>
+              <FaSearch />
+            </InputGroup.Text>
+          </InputGroup>
           {employees.length > 0 ? (
-            <Table striped bordered hover responsive>
+            <Table striped bordered hover responsive {...getTableProps()}>
               <thead>
-                <tr>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Date of Birth</th>
-                  <th>Start Date</th>
-                  <th>Department</th>
-                  <th>Street</th>
-                  <th>City</th>
-                  <th>State</th>
-                  <th>Zip Code</th>
-                </tr>
-              </thead>
-              <tbody>
-                {employees.map((employee, index) => (
-                  <tr key={index}>
-                    <td>{employee.firstName}</td>
-                    <td>{employee.lastName}</td>
-                    <td>{employee.dateOfBirth}</td>
-                    <td>{employee.startDate}</td>
-                    <td>{employee.department}</td>
-                    <td>{employee.street}</td>
-                    <td>{employee.city}</td>
-                    <td>{employee.state}</td>
-                    <td>{employee.zipCode}</td>
+                {headerGroups.map((headerGroup) => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                      <th
+                        {...column.getHeaderProps(
+                          column.getSortByToggleProps()
+                        )}
+                      >
+                        {column.render("Header")}
+                        <span>
+                          {column.isSorted
+                            ? column.isSortedDesc
+                              ? " 🔽"
+                              : " 🔼"
+                            : ""}
+                        </span>
+                      </th>
+                    ))}
                   </tr>
                 ))}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                  prepareRow(row);
+                  return (
+                    <tr {...row.getRowProps()}>
+                      {row.cells.map((cell) => {
+                        return (
+                          <td {...cell.getCellProps()}>
+                            {cell.render("Cell")}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
               </tbody>
             </Table>
           ) : (
